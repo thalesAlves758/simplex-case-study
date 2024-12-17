49e24ee8eb4fcde67d1279065866e55df80d6436
Thales Alves <thalesalves758@gmail.com>
2024-12-16 21:32:40 -0300

    refactor build initial table function

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 import data from './data.json' with { type: 'json' };
 
-const result = buildInitialTable(data);
+const result = buildInitialMatrix(data);
 
-console.log(result);
+console.table(result.matrix);
 
-function buildInitialTable(data) {
+function buildInitialMatrix(data) {
   const { inputConsumption, availability, lucrativity, preorders } = data;
 
   const inputConsumptionKeys = Object.keys(inputConsumption);
   const preordersKeys = Object.keys(preorders);
 
+  const baseLabel = ['base'];
   const variablesLabels = Object.keys(lucrativity);
   const artificialVariablesLabels = createArray(preordersKeys.length);
   const slackVariablesLabes = createArray(inputConsumptionKeys.length + preordersKeys.length);
   const variablesCount = variablesLabels.length;
+  const independentTermsLabel = ['b'];
 
   const coefficientsLines = [];
   const slackVariablesLines = [];
@@ -35,7 +37,7 @@ function buildInitialTable(data) {
       coefficientsline[lineVariableIndex] = inputConsumption[consumptionKey][variableConsumptionKey];
     });
 
-    let slackVariablesLine = createArray(consumptionsList.length);
+    let slackVariablesLine = createArray(slackVariablesLabes.length);
     slackVariablesLine[index] = 1;
     slackVariablesLabes[index] = `f${index + 1}`;
     basicVariablesLines.push([slackVariablesLabes[index]]);
@@ -44,7 +46,7 @@ function buildInitialTable(data) {
 
     coefficientsLines.push(coefficientsline);
     slackVariablesLines.push(slackVariablesLine);
-    artificialVariablesLines.push([]);
+    artificialVariablesLines.push(createArray(artificialVariablesLabels.length));
     independentTerms.push([availability[consumptionKey]]);
   });
 
@@ -53,12 +55,12 @@ function buildInitialTable(data) {
     let coefficientsLineIndex = variablesLabels.indexOf(preorderKey);
     coefficientsline[coefficientsLineIndex] = 1;
 
-    let slackVariablesLine = createArray(inputConsumptionKeys.length + preordersList.length);
+    let slackVariablesLine = createArray(slackVariablesLabes.length);
     slackVariablesLine[inputConsumptionKeys.length + index] = -1;
     slackVariablesLabes[inputConsumptionKeys.length + index] = `f${inputConsumptionKeys.length + index + 1}`;
     zLine.push(0);
 
-    let artificialVariablesLine = createArray(preordersList.length);
+    let artificialVariablesLine = createArray(artificialVariablesLabels.length);
     artificialVariablesLine[index] = 1;
     artificialVariablesLabels[index] = `a${index + 1}`;
     basicVariablesLines.push([artificialVariablesLabels[index]]);
@@ -75,8 +77,8 @@ function buildInitialTable(data) {
   setHeader(coefficientsLines, variablesLabels);
   setHeader(slackVariablesLines, slackVariablesLabes);
   setHeader(artificialVariablesLines, artificialVariablesLabels);
-  setHeader(independentTerms, ['b']);
-  setHeader(basicVariablesLines, ['base']);
+  setHeader(independentTerms, independentTermsLabel);
+  setHeader(basicVariablesLines, baseLabel);
 
   const finalMatrix = joinMatrixes(basicVariablesLines, coefficientsLines, slackVariablesLines, artificialVariablesLines, independentTerms);
   finalMatrix.push(['z', ...zLine]);
@@ -87,9 +89,9 @@ function buildInitialTable(data) {
     lineLabelIndex: 0,
     independentTermsColumnIndex: finalMatrix[0].length - 1,
     zLineIndex: finalMatrix.length - 1,
-    coefficientsIndexes: createOrderedArray(basicVariablesLines[0].length, variablesLabels.length),
-    slackVariablesIndexes: createOrderedArray((basicVariablesLines[0].length + variablesLabels.length), slackVariablesLabes.length),
-    artificialVariablesIndexes: createOrderedArray((basicVariablesLines[0].length + variablesLabels.length + slackVariablesLabes.length), artificialVariablesLabels.length)
+    coefficientsIndexes: createOrderedArray(baseLabel.length, variablesLabels.length),
+    slackVariablesIndexes: createOrderedArray((baseLabel.length + variablesLabels.length), slackVariablesLabes.length),
+    artificialVariablesIndexes: createOrderedArray((baseLabel.length + variablesLabels.length + slackVariablesLabes.length), artificialVariablesLabels.length)
   };
 }
 
@@ -98,22 +100,12 @@ function createOrderedArray(startNumber, size, step = 1) {
 }
 
 function createArray(length) {
-  return Array(length).fill();
-}
-
-function fillMatrix(matrix, value = 0) {
-  const linesLength = matrix.reduce((length, line) => (line.length > length ? line.length : length), 0);
-
-  return matrix.map(line => {
-    return Array(linesLength).fill(value).map((_, index) => line[index] ?? value);
-  });
+  return Array(length).fill(0);
 }
 
 function joinMatrixes(...matrixes) {
   return matrixes.reduce((result, matrixToAdd) => {
-    let filledMatrix = fillMatrix(matrixToAdd);
-
-    filledMatrix.forEach((line, lineIndex) => {
+    matrixToAdd.forEach((line, lineIndex) => {
       if (!result[lineIndex]?.length) {
         result[lineIndex] = line;
         return;
