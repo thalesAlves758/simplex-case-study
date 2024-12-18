5e16ae401470a5e206fcff2a13fd0fa718da1bf9
Thales Alves <thalesalves758@gmail.com>
2024-12-17 23:42:43 -0300

    add fixNumber function

--- utils.js
+++ utils.js
@@ -10,4 +10,8 @@ function copyObject(object) {
   return JSON.parse(JSON.stringify(object));
 }
 
-export { createArray, createOrderedArray, copyObject };
+function fixNumber(number) {
+  return Number(number.toFixed(2));
+}
+
+export { createArray, createOrderedArray, copyObject, fixNumber };
