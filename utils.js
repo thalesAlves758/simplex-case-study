39b8999e5c504c52653fd17cfefbd0666308a54e
Thales Alves <thalesalves758@gmail.com>
2024-12-18 01:26:47 -0300

    add renderTable function

--- utils.js
+++ utils.js
@@ -14,4 +14,39 @@ function fixNumber(number) {
   return Number(number.toFixed(2));
 }
 
-export { createArray, createOrderedArray, copyObject, fixNumber };
+function renderTable(matrix, headerIndex, columnLabelIndex, tableTitle, { lineIndexes = [], columnIndexes = [], lineBgHighlight = "blue", columnBgHighlight = "blue" } = {}) {
+  let tableEl = ``;
+
+  matrix.forEach((line, lineIndex) => {
+    let lineHighlightClassName = lineIndexes.includes(lineIndex) ? lineBgHighlight : '';
+
+    if (lineIndex === headerIndex) {
+      tableEl += `
+        <tr>
+          ${line.map((label, columnIndex) => (`<th class="bold ${lineHighlightClassName} ${isHighlightColumn(columnIndexes, columnIndex) ? columnBgHighlight : ''}">${label}</th>`)).join('')}
+        </tr>
+      `;
+      return;
+    }
+
+    tableEl += `
+      <tr>
+        ${line.map((label, columnIndex) => (`<td class="${columnIndex === columnLabelIndex ? 'bold' : ''} ${lineHighlightClassName} ${isHighlightColumn(columnIndexes, columnIndex) ? columnBgHighlight : ''}">${label}</td>`)).join('')}
+      </tr>
+    `;
+  });
+
+  document.body.innerHTML += `
+    <table>
+      <caption>${tableTitle}</caption>
+      ${tableEl}
+    </table>
+    <hr>
+  `;
+}
+
+function isHighlightColumn(hightlightColumnsIndexes, columnIndex) {
+  return hightlightColumnsIndexes.includes(columnIndex);
+}
+
+export { createArray, createOrderedArray, copyObject, fixNumber, renderTable };
